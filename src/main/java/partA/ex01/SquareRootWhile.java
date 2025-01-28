bde078ebaba94ef9172c06e9189e00b9b43b440a
Clamjosh <Clamjosh1@users.noreply.github.com>
2025-01-28 00:18:57 -0500

    I didnt finish, but i forgot to push

--- src/main/java/partA/ex01/SquareRootWhile.java
+++ src/main/java/partA/ex01/SquareRootWhile.java
@@ -4,7 +4,18 @@ import java.util.Scanner;
 
 public class SquareRootWhile {
     public static String squareRoot(){
+        Scanner scanner = new Scanner(System.in);
+        double number = -1;
         String response = "";
+
+        while (number < 0) {
+            System.out.print("Type a non-negative integer: ");
+            number = scanner.nextDouble();
+
+            if (number < 0) {
+                System.out.print("Invalid number, try again: ");
+            }
+        }
 
         return response;
     }
